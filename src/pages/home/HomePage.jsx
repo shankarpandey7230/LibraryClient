e455e51e2306c152c77407b9ab827cd584c24f09
shankarpandey7230 <shankarpandey7230@gmail.com>
2025-12-24 13:14:51 +1100

    homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,7 +1,30 @@
+import { CustomCarousel } from "@components/customCarousel/CustomCarousel";
+import { BestRead } from "@components/pageSection/BestRead";
+import { JustInSection } from "@components/pageSection/JustInSection";
+import { Recommendation } from "@components/pageSection/Recommendation";
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container className="mb-4">
+      <Row>
+        <Col>
+          {/* Hero section  */}
+          <CustomCarousel />
+
+          {/* Just in Section  */}
+          <JustInSection />
+
+          {/* Best read Section  */}
+          <BestRead />
+
+          {/* Recc. Section  */}
+          <Recommendation />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default HomePage;
